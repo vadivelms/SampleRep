46366bd202afc8bc40e71fe08b40add1e2afbf65
Vadivel Manjanaicker <vadivelmso@gmail.com>
2023-07-27 01:39:40 +0000

    libraries

---
+++ CS50/itune.py
@@ -0,0 +1,13 @@
+import requests
+import sys
+import json
+
+if len(sys.argv) != 2:
+    sys.exit()
+response = requests.get("https://itunes.apple.com/search?term="+sys.argv[1]+"&limit=50")
+# print(json.dumps(response.json(),indent=2))
+
+o = response.json()
+
+for request in o["results"]:
+    print(request["trackName"])
--- CS50/say.py
+++ CS50/say.py
@@ -1,3 +1,8 @@
-import cowsay
+# import cowsay
 
-cowsay.trex("Kumar")
+# cowsay.trex("Kumar")
+import sys
+from sayings import hello
+
+if len(sys.argv)==2:
+    hello(sys.argv[1])
